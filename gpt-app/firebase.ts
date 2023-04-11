fffa8553237209508ab633d26bdcced7a776a286
Lee Sang Ho <lee.sangho@startuptechnology.com>
2023-04-11 16:33:51 +0900

    Revert "move value to env"
    This reverts commit a9a4b57866ed2ee18ec12d63755910335cb4d44b.

--- gpt-app/firebase.ts
+++ gpt-app/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_APIKEY,
-  authDomain: process.env.FIREBASE_AUTHDOMAIN,
-  projectId: process.env.FIREBASE_PROJECTID,
-  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
-  appId: process.env.FIREBASE_APPID,
-  measurementId: process.env.FIREBASE_MEASUREMENTID,
+  apiKey: 'AIzaSyCW6xcgRMKaI9pYa3X7n9SrByajTzHBlTw',
+  authDomain: 'chat-gpt-app-1a9e8.firebaseapp.com',
+  projectId: 'chat-gpt-app-1a9e8',
+  storageBucket: 'chat-gpt-app-1a9e8.appspot.com',
+  messagingSenderId: '915702271224',
+  appId: '1:915702271224:web:d08b8b1ecf087bac67bf0d',
+  measurementId: 'G-4L6P70XBV4',
 }
 
 // Initialize Firebase
